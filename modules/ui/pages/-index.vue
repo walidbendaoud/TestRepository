3a16b731046336420c8aae2210d6bbd2fd913eca
walidbendaoud <93764187+walidbendaoud@users.noreply.github.com>
2022-02-17 10:25:06 +0100

    Initialize renovate/node-14.x

---
+++ modules/ui/pages/-index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nuxt-link to="/product?theme=custom">
+      <a-button type="primary"> test routes middleware </a-button>
+    </nuxt-link>
+
+    <nuxt-link to="/users">
+      <a-button type="primary"> users </a-button>
+    </nuxt-link>
+
+    welcome to nuxt DDD
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  
+})
+</script>
